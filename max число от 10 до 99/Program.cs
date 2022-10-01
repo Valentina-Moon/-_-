ef08cf24423122bed2c9d0b1950784966e48f8ca
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-01 17:17:07 +0300

    max цифра

---
+++ max число от 10 до 99/Program.cs
@@ -0,0 +1,18 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write(" наибольшая цифра числа ");
+int[] arr = new int[99];
+Random rand = new Random();
+for (int i = 10; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(99);
+    Console.WriteLine(arr[i]+" ");
+}
+int max = arr[10];
+for (int i = 10; i < arr.Length; i++)
+{
+if (arr[i] > max)
+{
+    max = arr[i];
+}
+}
+Console.WriteLine(" Max:" + max);
